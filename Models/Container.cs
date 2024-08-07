13f6c959600182f8d5ef8fe620fe9b7ea49cc985
usesarch <declan.riedel@protonmail.com>
2024-08-07 14:23:26 +0200

    tests!!

---
+++ Models/Container.cs
@@ -0,0 +1,10 @@
+namespace ContainerInspectionApp.Models
+{
+    public class Container
+    {
+        public string ContainerId { get; set; }
+        public string ContainerType { get; set; }
+        public string Contents { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
--- Services/ContainerTableOperations.cs
+++ Services/ContainerTableOperations.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Configuration;
 using Npgsql;
 using System;
 using System.Threading.Tasks;
+using ContainerInspectionApp.Models;
 
 namespace ContainerInspectionApp.Services
 {
@@ -65,6 +66,78 @@ namespace ContainerInspectionApp.Services
             }
 
             return data.ToArray();
+        }
+
+        public async Task<bool> InsertContainer(string containerId, string containerType, string contents, DateTime dateAdded)
+        {
+            var connectionString = GetConnectionString();
+
+            await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+            try
+            {
+                await using var cmd = dataSource.CreateCommand("INSERT INTO containers (container_id, container_type, contents, date_added) VALUES ($1, $2, $3, $4)");
+                cmd.Parameters.AddWithValue(containerId);
+                cmd.Parameters.AddWithValue(containerType);
+                cmd.Parameters.AddWithValue(contents);
+                cmd.Parameters.AddWithValue(dateAdded);
+                await cmd.ExecuteNonQueryAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> ResetDatabase()
+        {
+            var connectionString = GetConnectionString();
+
+            await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+            try
+            {
+                await using var cmd = dataSource.CreateCommand(@"
+                    DROP TABLE IF EXISTS containers;
+                    CREATE TABLE containers (
+                        id SERIAL PRIMARY KEY,
+                        container_id VARCHAR(50) NOT NULL,
+                        container_type VARCHAR(50) NOT NULL,
+                        contents TEXT NOT NULL,
+                        date_added DATE NOT NULL
+                    );");
+                await cmd.ExecuteNonQueryAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<Container>> GetAllContainers()
+        {
+            var connectionString = GetConnectionString();
+
+            await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+            var containers = new List<Container>();
+
+            await using var cmd = dataSource.CreateCommand("SELECT container_id, container_type, contents, date_added FROM containers ORDER BY date_added DESC");
+            await using var reader = await cmd.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                containers.Add(new Container
+                {
+                    ContainerId = reader.GetString(0),
+                    ContainerType = reader.GetString(1),
+                    Contents = reader.GetString(2),
+                    DateAdded = reader.GetDateTime(3)
+                });
+            }
+
+            return containers;
         }
     }
 }
